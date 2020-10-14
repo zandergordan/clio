48507d429d5aefe591d644232f2f40976366d5b2
Zander Gordan <me@zandergordan.com>
2020-10-13 21:58:31 -0400

    Use multiple pins, each assigned to a MIDI note

--- blink.py
+++ blink.py
@@ -7,31 +7,52 @@ from pymata4 import pymata4
 """
 Setup a pin for digital output and a MIDI port for input.
 Note on messages turn on the pin, likewise for note offs.
+Each Arduino pin to be used is assigned to a particular
+MIDI note.
 """
 
-# some globals
-DIGITAL_PIN = 6  # arduino pin number
+"""
+Set arduino pin numbers connected to valves, and the MIDI
+notes which will connect to them through the virtual port
+opened by MIDO
+"""
+DIGITAL_PINS = {60:6, # note 60 is MIDI for C3
+                62:7,
+                64:8,
+                65:13}
 
 
-def blink(my_board, pin):
+def blink(my_board, pins):
     """
     This function will toggle a digital pin.
     :param my_board: an PymataExpress instance
-    :param pin: pin to be controlled
+    :param pins: dictionary mapping MIDI notes to pins
     """
 
     # set the pin mode
-    my_board.set_pin_mode_digital_output(pin)
+    for (note,pin) in pins.items():
+        my_board.set_pin_mode_digital_output(pin)
 
     # open a MIDI port and control the LED
     clio_port = mido.open_input('Clio', virtual=True)
     while True:
         message = clio_port.receive()
-        print(message)
-        if message.type == 'note_on':
-            my_board.digital_write(pin, 1)
-        elif message.type == 'note_off':
-            my_board.digital_write(pin, 0)
+        m_type = message.type
+        # if message is not a note on or note off type, ignore it
+        if not m_type in ['note_on', 'note_off']:
+            continue
+        m_note = message.note
+        # if message uses unassigned note, ignore it
+        try:
+            m_pin = pins[m_note]
+        except KeyError:
+            print("unassigned note: "+str(m_note))
+            continue
+        # send message to Arduino
+        if m_type == 'note_on':
+            my_board.digital_write(m_pin, 1)
+        elif m_type == 'note_off':
+            my_board.digital_write(m_pin, 0)
         else:
             pass
 
@@ -39,7 +60,7 @@ def blink(my_board, pin):
 
 board = pymata4.Pymata4()
 try:
-    blink(board, DIGITAL_PIN)
+    blink(board, DIGITAL_PINS)
 except KeyboardInterrupt:
     board.shutdown()
     sys.exit(0)
